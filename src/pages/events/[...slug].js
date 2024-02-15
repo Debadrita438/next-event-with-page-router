2c6295260f00a26acd6c4a2097762e4524de651b
debadrita205 <debadrita@technoexponent.com>
2024-02-15 18:07:53 +0530

    project structure done

---
+++ src/pages/events/[...slug].js
@@ -0,0 +1,7 @@
+export default function filteredEventsPage() {
+  return (
+    <div>
+      <h1>The Filtered Events Page</h1>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,9 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <></>;
+  return (
+    <div>
+      <h1>The Home Page</h1>
+    </div>
+  );
 }
